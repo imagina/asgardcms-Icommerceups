3fc7839123f1c70b1800217929b746af4f025a71
William Verde <williamverde10@gmail.com>
2019-02-26 16:30:08 -0400

    files lang

---
+++ Resources/lang/es/icommerceups.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+    'single' => 'UPS',
+    'description' => 'La descripcion del Modulo',
+    'list resource' => 'List icommerceups',
+    'create resource' => 'Create icommerceups',
+    'edit resource' => 'Edit icommerceups',
+    'destroy resource' => 'Destroy icommerceups',
+    'title' => [
+        'icommerceups' => 'Icommerceups',
+        'create icommerceups' => 'Create a icommerceups',
+        'edit icommerceups' => 'Edit a icommerceups',
+    ],
+    'button' => [
+        'create icommerceups' => 'Create a icommerceups',
+    ],
+    'table' => [
+        'accessKey' => 'Access Key',
+        'userId' => 'User Id',
+        'password' => 'Password',
+        'shipperPostalCode' => 'Shipper Postalcode',
+        'shipperStateCode' => 'Shipper Statecode',
+        'shipperCountryCode' => 'Shipper Countrycode',
+        'mode' => 'Mode',
+    ],
+    'form' => [
+    ],
+    'messages' => [
+        'msjini' => "Debes ingresar el Codigo postal y el Pais"
+    ],
+    'validation' => [
+    ],
+];
